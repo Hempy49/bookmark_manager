f837ecbe51fec030373222f5eec014ffb8710305
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-29 11:53:30 +0100

    Filtering by tag

--- app/app.rb
+++ app/app.rb
@@ -16,10 +16,17 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(tag: params[:tag])
+    link = Link.create(url: params[:url], title: params[:title])
+    tag = Tag.first_or_create(name: params[:tag])
     link.tags << tag
     link.save
     redirect '/links'
   end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:tag])
+    @links = tag ? tag.links : []
+    erb :links
+  end
+
 end
--- spec/tagging_links_spec.rb
+++ spec/tagging_links_spec.rb
@@ -7,6 +7,18 @@ feature 'adding tags to links' do
     click_button("Create link")
     visit '/links'
     link = Link.first
-    expect(link.tags.map(&:tag)).to include('Browsers')
+    expect(link.tags.map(&:name)).to include('Browsers')
+  end
+end
+
+feature 'filter links by tag' do
+  scenario 'find bubbles tag' do
+    visit '/links/new'
+    fill_in('url', :with => 'http://bubbleology.co.uk/')
+    fill_in('title', :with => 'bubbleology')
+    fill_in('tag', :with => 'bubbles')
+    click_button("Create link")
+    link = Link.first
+    expect(link.tags.map(&:name)).to include 'bubbles'
   end
 end
